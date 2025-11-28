3732519819e71c2ebcac163ef230dc5bd542913a
Arpan Mahara <amaha038@fiu.edu>
2025-11-27 20:59:20 -0500

    Updating UNET parameters

--- src/engine.py
+++ src/engine.py
@@ -50,17 +50,17 @@ def main(rank, world_size, args):
 
     # -------- Model Setup --------
     model = UNetModel(
-        in_channels=6,
-        model_channels=96,
-        out_channels=3,
-        num_res_blocks=2,
-        attention_resolutions=(4, 8),
+        in_channels=8,
+        model_channels=192,
+        out_channels=4,
+        num_res_blocks=3,
+        attention_resolutions=(2, 4, 8),
         dropout=0.1,
         channel_mult=(1, 2, 3, 4),
         num_classes=None,
         use_checkpoint=False,
         num_heads=4,
-        num_head_channels=48,
+        num_head_channels=64,
         use_scale_shift_norm=True,
         resblock_updown=True,
         use_new_attention_order=True,
