04a0e042991bd78c519ef3df0daa50e548fc0918
Arpan Mahara <amaha038@fiu.edu>
2025-11-29 21:34:34 -0500

    Adding scaling factor as args

--- train.py
+++ train.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--num_workers", type=int, default=4)
     parser.add_argument("--lr", type=float, default=3e-4)
-    parser.add_argument("--eval_interval", type=int, default=10, help="Evaluate and save 10 samples every N epochs")
+    parser.add_argument("--eval_interval", type=int, default=10,
+                        help="Evaluate and save samples every N epochs")
     parser.add_argument("--no_fid", action="store_true",
                         help="Skip FID evaluation during training (still saves checkpoints).")
+    parser.add_argument("--scale_factor", type=float, default=0.18215)
     args = parser.parse_args()
 
     rank = int(os.environ["RANK"])
